c18d54c735913f53418d3d809315719e91d20e6c
Kostiantyn Zhereb <zhereb@gmail.com>
2019-02-15 12:10:38 +0200

    Check function values for different n, x, y
    For n>=4, we will only get powers of 2

--- code-demo/stack_queue_recursion/ackermann.cpp
+++ code-demo/stack_queue_recursion/ackermann.cpp
@@ -10,7 +10,9 @@ struct mem {
 struct stack_ackermann {
 	mem st[STACK_SIZE];
 	int current_index;
-	stack_ackermann() { current_index = STACK_SIZE; }
+	stack_ackermann() {
+		current_index = STACK_SIZE;
+	}
 };
 
 int acc(int, int, int);
@@ -22,11 +24,26 @@ int smacc(int, int);
 
 int main() {
 	int n, x, y;
-	cout << "Function Ackermann n= x= y=" << endl;
-	cin >> n >> x >> y;
-	cout << endl;
-	cout << "Function value (recursive) = " << accrec(n, x, y) << endl;
-	cout << "Function value (stack)     = " << acc(n, x, y) << endl;
+
+	//cin >> n >> x >> y;
+	//cout << endl;
+	for (n = 0; n < 6; n++) {
+		for (x = 0; x < 5; x++) {
+			for (y = 0; y < 5; y++) {
+				cout << "Function Ackermann n="<<n<<" x="<<x<<" y="<<y<< endl;
+				//cout << "Function value (recursive) = " << accrec(n, x, y)
+				//		<< endl;
+				try {
+					cout << "Function value (stack)     = " << acc(n, x, y) << endl;
+				}
+				catch (out_of_range& e) {
+					cout<<e.what()<<endl;
+				}
+
+			}
+		}
+	}
+
 	//system("pause");
 	return 0;
 }
@@ -48,15 +65,15 @@ int acc(int ni, int xi, int yi) {
 	push(stack, ni, xi, yi);
 	do {
 
-		peek(stack,n, x, y);
+		peek(stack, n, x, y);
 		if (n && y)
-			push(stack,n, x, y - 1);
+			push(stack, n, x, y - 1);
 		else {
 			t = smacc(n, x);
 			++stack.current_index;
 			if (!stack_empty(stack)) {
-				pop(stack,n,x,y);
-				push(stack,n - 1, t, x);
+				pop(stack, n, x, y);
+				push(stack, n - 1, t, x);
 			}
 		}
 	} while (!stack_empty(stack));
@@ -87,20 +104,21 @@ void push(stack_ackermann & stack, int ni, int xi, int yi) {
 		stack.st[stack.current_index].ns = ni;
 		stack.st[stack.current_index].xs = xi;
 		stack.st[stack.current_index].ys = yi;
-		cout<<"added values n="<<ni<<", x="<<xi<<", y="<<yi<<endl;
-	} else
-		cout << "Stack full" << endl;
+		//cout<<"added values n="<<ni<<", x="<<xi<<", y="<<yi<<endl;
+	} else {
+		throw out_of_range("Stack full");
+	}
 }
 //--------------------------------
 //читання з стека st трійки
-void peek(stack_ackermann const & stack,int& n, int& x, int& y) {
+void peek(stack_ackermann const & stack, int& n, int& x, int& y) {
 	n = stack.st[stack.current_index].ns;
 	x = stack.st[stack.current_index].xs;
 	y = stack.st[stack.current_index].ys;
-	cout<<"used values n="<<n<<", x="<<x<<", y="<<y<<endl;
+	//cout<<"used values n="<<n<<", x="<<x<<", y="<<y<<endl;
 }
 
 void pop(stack_ackermann & stack, int& n, int& x, int& y) {
-	peek(stack, n,x,y);
+	peek(stack, n, x, y);
 	stack.current_index++;
 }
