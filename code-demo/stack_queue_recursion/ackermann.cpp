92c9ce8a44dab9b803bb8b3951454f5c39fc1f46
Kostiantyn Zhereb <zhereb@gmail.com>
2019-02-15 11:52:30 +0200

    Refactor to remove all global variables

--- code-demo/stack_queue_recursion/ackermann.cpp
+++ code-demo/stack_queue_recursion/ackermann.cpp
@@ -4,13 +4,19 @@ using namespace std;
 const int STACK_SIZE = 100;
 struct mem {
 	int ns, xs, ys;
-} st[STACK_SIZE];
-int current_index = STACK_SIZE;
+};
+//int current_index = STACK_SIZE;
+
+struct stack_ackermann {
+	mem st[STACK_SIZE];
+	int current_index;
+	stack_ackermann() { current_index = STACK_SIZE; }
+};
 
 int acc(int, int, int);
-void push(int, int, int);
-void peek(int&, int&, int&);
-void pop(int&, int&, int&);
+void push(stack_ackermann &, int, int, int);
+void peek(stack_ackermann const &, int&, int&, int&);
+void pop(stack_ackermann &, int&, int&, int&);
 int accrec(int, int, int);
 int smacc(int, int);
 
@@ -30,29 +36,30 @@ int accrec(int n, int x, int y) {
 	return (n && y ? accrec(n - 1, accrec(n, x, y - 1), x) : smacc(n, x));
 }
 
-bool stack_empty() {
-	return current_index >= STACK_SIZE;
+bool stack_empty(stack_ackermann const & stack) {
+	return stack.current_index >= STACK_SIZE;
 }
 
 //--------------------------------
 //нерекурсивна функція Аккермана
 int acc(int ni, int xi, int yi) {
 	int t, n, x, y;
-	push(ni, xi, yi);
+	stack_ackermann stack;
+	push(stack, ni, xi, yi);
 	do {
 
-		peek(n, x, y);
+		peek(stack,n, x, y);
 		if (n && y)
-			push(n, x, y - 1);
+			push(stack,n, x, y - 1);
 		else {
 			t = smacc(n, x);
-			++current_index;
-			if (!stack_empty()) {
-				pop(n,x,y);
-				push(n - 1, t, x);
+			++stack.current_index;
+			if (!stack_empty(stack)) {
+				pop(stack,n,x,y);
+				push(stack,n - 1, t, x);
 			}
 		}
-	} while (!stack_empty());
+	} while (!stack_empty(stack));
 	return t;
 }
 
@@ -75,23 +82,25 @@ int smacc(int n, int x) {
 
 //--------------------------------
 //додавання в стек st нової трійки
-void push(int ni, int xi, int yi) {
-	if (current_index--) {
-		st[current_index].ns = ni;
-		st[current_index].xs = xi;
-		st[current_index].ys = yi;
+void push(stack_ackermann & stack, int ni, int xi, int yi) {
+	if (stack.current_index--) {
+		stack.st[stack.current_index].ns = ni;
+		stack.st[stack.current_index].xs = xi;
+		stack.st[stack.current_index].ys = yi;
+		cout<<"added values n="<<ni<<", x="<<xi<<", y="<<yi<<endl;
 	} else
 		cout << "Stack full" << endl;
 }
 //--------------------------------
 //читання з стека st трійки
-void peek(int& n, int& x, int& y) {
-	n = st[current_index].ns;
-	x = st[current_index].xs;
-	y = st[current_index].ys;
+void peek(stack_ackermann const & stack,int& n, int& x, int& y) {
+	n = stack.st[stack.current_index].ns;
+	x = stack.st[stack.current_index].xs;
+	y = stack.st[stack.current_index].ys;
+	cout<<"used values n="<<n<<", x="<<x<<", y="<<y<<endl;
 }
 
-void pop(int& n, int& x, int& y) {
-	peek(n,x,y);
-	current_index++;
+void pop(stack_ackermann & stack, int& n, int& x, int& y) {
+	peek(stack, n,x,y);
+	stack.current_index++;
 }
