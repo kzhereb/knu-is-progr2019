a2ef727000e4b430d1b0fd1c229fd4b97e5656e1
Kostiantyn Zhereb <zhereb@gmail.com>
2019-02-22 11:31:11 +0200

    Generate random numbers
    Fix for MinGW random_device

--- code-demo/list/sparse_vector-k18lesson7.cpp
+++ code-demo/list/sparse_vector-k18lesson7.cpp
@@ -1,10 +1,33 @@
 //скал¤рний добуток вектор≥в
 #include <iostream>
+#include <random>
+
+#include <ctime>
+
 using namespace std;
 
 const int NN=10000, KK=800;
 
+int generate_random(){
+    static std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    static std::seed_seq seed { rd(), static_cast<unsigned int>(time(nullptr))}; // in case random_device is not implemented, fallback to time(0)
+    static std::mt19937_64 gen(seed); //Standard mersenne_twister_engine seeded with rd()
+
+    static std::uniform_int_distribution<> dis(1, 6);
+
+    return dis(gen);
+
+}
+
+
 int main() {
+
+  for (int i=0;i<100;i++) {
+	  cout<<generate_random()<<" ";
+  }
+  cout<<endl;
+  return 0;
+
   int i, inp, sum;
   struct {int nm, val;} M[KK], *p;
   p = M;
