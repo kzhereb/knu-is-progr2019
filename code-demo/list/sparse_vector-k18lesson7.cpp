58f91b78c0fd4a82f4d61b25ff5887ee7587e4d6
Kostiantyn Zhereb <zhereb@gmail.com>
2019-02-22 11:43:35 +0200

    Refactoring names, generate 92% zeroes

--- code-demo/list/sparse_vector-k18lesson7.cpp
+++ code-demo/list/sparse_vector-k18lesson7.cpp
@@ -6,14 +6,17 @@
 
 using namespace std;
 
-const int NN=10000, KK=800;
+const int MAX_SIZE=10000, MAX_CAPACITY=800;
 
 int generate_random(){
     static std::random_device rd;  //Will be used to obtain a seed for the random number engine
     static std::seed_seq seed { rd(), static_cast<unsigned int>(time(nullptr))}; // in case random_device is not implemented, fallback to time(0)
     static std::mt19937_64 gen(seed); //Standard mersenne_twister_engine seeded with rd()
 
-    static std::uniform_int_distribution<> dis(1, 6);
+    static std::uniform_real_distribution<> real_dis;
+    static std::uniform_int_distribution<> dis(-100, 100);
+
+    if (real_dis(gen)<=0.92) {return 0;}
 
     return dis(gen);
 
@@ -29,16 +32,16 @@ int main() {
   return 0;
 
   int i, inp, sum;
-  struct {int nm, val;} M[KK], *p;
-  p = M;
-  cout << "M : " << endl;
-  for (i=0; i<NN; i++) {
-  	  cin >> inp;
+  struct {int nm, val;} sparse_array[MAX_CAPACITY], *p;
+  p = sparse_array;
+  cout << "sparse_array : " << endl;
+  for (i=0; i<MAX_SIZE; i++) {
+  	  inp = generate_random();
   	  if (p->val = inp) (p++)->nm = i;
   	}
-  p->nm = NN; sum = 0; p = M;
+  p->nm = MAX_SIZE; sum = 0; p = sparse_array;
   cout << "N : " << endl;
-  for (i=0; i<NN; i++) {
+  for (i=0; i<MAX_SIZE; i++) {
     cin >> inp;
   	if (i == p->nm) sum += (p++)->val * inp;
   }
