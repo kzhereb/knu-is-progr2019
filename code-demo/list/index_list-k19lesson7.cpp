052b0db7c62d0bbe4a1672a539583b5043b22083
Kostiantyn Zhereb <zhereb@gmail.com>
2019-02-22 13:30:20 +0200

    Refactored code, another implementation

--- code-demo/list/index_list-k19lesson7.cpp
+++ code-demo/list/index_list-k19lesson7.cpp
@@ -6,10 +6,90 @@ typedef struct Node {
 	Node *next;
 } Listn, *Listp;
 
+struct List {
+	Node* first;
+	Node* last;
+	List() {
+		first=last=nullptr;
+	}
+};
+
+struct PseudoHashList {
+	static const int MAX_BUCKETS = 100;
+	List buckets[MAX_BUCKETS];
+};
+
 void index(Listp x[100]);
 void lprint(Listp);
 
+
+void add_item_to_list(List& list, int item) {
+	Node* new_node = new Node;
+	new_node->dat = item;
+	new_node->next = nullptr;
+
+	if (list.last) {
+		list.last->next = new_node;
+		list.last = new_node;
+	} else {
+		list.first = list.last = new_node;
+	}
+
+}
+
+int pseudo_hash(int number) {
+	return number % PseudoHashList::MAX_BUCKETS;
+}
+
+void add_item(PseudoHashList & list, int item) {
+	int hash = pseudo_hash(item);
+	add_item_to_list(list.buckets[hash], item);
+}
+
+void print_list(List& list) {
+	Node* curr_node = list.first;
+	while(curr_node) {
+		cout<<curr_node->dat<<" ";
+		curr_node = curr_node->next;
+	}
+}
+
+bool is_empty_list(List& list) {
+	return list.first == nullptr;
+}
+
+void print_pseudo_hash_list(PseudoHashList & list) {
+	for(int i=0; i<PseudoHashList::MAX_BUCKETS;i++ ){
+		List& curr_list = list.buckets[i];
+		if (!is_empty_list(curr_list)) {
+			cout<<i<<": ";
+			print_list(curr_list);
+			cout<<endl;
+		}
+	}
+}
+
+void input_into_hash(PseudoHashList & list) {
+	cout << "Enter list (0 to end) : " << endl;
+
+
+	while(true) {
+		int curr_val;
+		cin>> curr_val;
+		if (curr_val == 0) {break;}
+		add_item(list, curr_val);
+	}
+}
+
 int main() {
+
+	PseudoHashList list;
+	input_into_hash(list);
+	print_pseudo_hash_list(list);
+	return 0;
+
+
+
 	Listp xx[100];
 	index(xx);
 	cout << "Rezult list : " << endl;
@@ -28,7 +108,7 @@ void index(Listp x[100]) {
 	Listp t;
 	int inp, i;
 	for (i = 0; i < 100; i++)
-		x[i] = NULL;  //порожня структура
+		x[i] = nullptr;  //порожня структура
 	//побудова сукупності циклічних списків з покажчиками в x[i]
 	cout << "List : " << endl;
 	cin >> inp;
