b17c865e8d24adfaa1bc25075c4bb2d823d305d6
Kostiantyn Zhereb <zhereb@gmail.com>
2019-04-26 14:17:01 +0300

    Implemented Kruskal and DJP algorithms for minimal spanning tree

--- code-demo/graph/spanningtree_k19lesson14.cpp
+++ code-demo/graph/spanningtree_k19lesson14.cpp
@@ -4,15 +4,17 @@
  *  Created on: Apr 26, 2019
  *      Author: KZ
  */
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using std::cout;
 using std::endl;
 
-//copypaste
 const int MAX_VERTICES = 20;
 
 struct GraphMatr {
-	bool adjacent[MAX_VERTICES][MAX_VERTICES];
+	int adjacent[MAX_VERTICES][MAX_VERTICES];
 	int vertex_count;
 	bool is_oriented=false;
 	GraphMatr(int vertices) {
@@ -46,22 +48,126 @@ void print_edges(const GraphMatr& graph) {
 		for (; j<graph.vertex_count;j++) {
 			if (graph.adjacent[i][j]) {
 				cout<<"("<<i<<","<<j<<",w="<<graph.adjacent[i][j]<<") ";
+
 			}
 		}
 	}
 	cout<<endl;
 }
-//copypaste-end
 
+struct Edge {
+	int from;
+	int to;
+	int weight;
+	Edge(int _from, int _to, int w=1) {
+		from=_from;
+		to = _to;
+		weight = w;
+	}
+};
 
-int main() {
-	GraphMatr graph(5);
-	add_edge(graph,0,1);
-	add_edge(graph,2,3);
-	add_edge(graph,0,4);
-	print_edges(graph);
-	return 0;
+std::vector<Edge> get_edges_sorted(GraphMatr const & graph) {
+	std::vector<Edge> result;
+	for(int i=0;i<graph.vertex_count;i++) {
+		for (int j=i+1;j<graph.vertex_count;j++) {
+			if (graph.adjacent[i][j]) {
+				result.emplace_back(Edge{i,j,graph.adjacent[i][j]});
+			}
+		}
+	}
+	std::sort(result.begin(),result.end(),
+			[](Edge e1, Edge e2) {
+				if (e1.weight<e2.weight) return true;
+				return false;
+	});
+	return result;
 }
 
+GraphMatr min_spanning_tree_kruskal(GraphMatr const & graph ) {
+	GraphMatr result(graph.vertex_count);
+	int which_tree[MAX_VERTICES];
+	for(int i=0;i<graph.vertex_count;i++) {
+		which_tree[i]=i;
+	}
+	auto edges = get_edges_sorted(graph);
+	for(auto edge:edges) {
+		if (which_tree[edge.from]!=which_tree[edge.to]) {
+			add_edge(result,edge.from,edge.to,edge.weight);
+			for (int i=0;i<graph.vertex_count;i++) {
+				if(which_tree[i] == which_tree[edge.to]) {
+					which_tree[i] = which_tree[edge.from];
+				}
+			}
 
+		}
+	}
+	return result;
+}
 
+GraphMatr min_spanning_tree_djp(GraphMatr const & graph ) {
+	GraphMatr result(graph.vertex_count);
+	int cheapest_cost[MAX_VERTICES];
+	int cheapest_edge[MAX_VERTICES];
+	bool already_picked[MAX_VERTICES];
+	int vertices_in_result=0;
+	for(int i=0;i<graph.vertex_count;i++) {
+		cheapest_cost[i]=INT32_MAX;
+		cheapest_edge[i]=-1;
+		already_picked[i]=false;
+	}
+	//already_picked[0]=true;
+	while (vertices_in_result<graph.vertex_count){
+		int min_cost = INT32_MAX,min_vertex=0;
+
+		for(int i=0;i<graph.vertex_count;i++) {
+			if (!already_picked[i] && cheapest_cost[i]<min_cost) {
+				min_vertex=i;
+				min_cost= cheapest_cost[i];
+			}
+		}
+		cout<<"Picked "<<min_vertex<<endl;
+		vertices_in_result++;
+		already_picked[min_vertex]=true;
+		if(cheapest_edge[min_vertex]!=-1) {
+			add_edge(result,min_vertex,cheapest_edge[min_vertex],
+					graph.adjacent[min_vertex][cheapest_edge[min_vertex]]);
+		}
+		for(int i=0;i<graph.vertex_count;i++) {
+			if (!already_picked[i]
+				&& graph.adjacent[min_vertex][i]
+				&& graph.adjacent[min_vertex][i] < cheapest_cost[i]) {
+				cheapest_cost[i] = graph.adjacent[min_vertex][i];
+				cheapest_edge[i] = min_vertex;
+			}
+		}
+		for(int i=0;i<graph.vertex_count;i++) {
+			cout<<i<<": cost="<<cheapest_cost[i]<<",edge="<<cheapest_edge[i]<<endl;
+		}
+	}
+
+	return result;
+}
+
+int main() {
+	GraphMatr graph(6);
+	//GraphStruct graph(5);
+	add_edge(graph,0,1,1);
+	add_edge(graph,0,3,4);
+	add_edge(graph,0,4,3);
+	add_edge(graph,1,3,4);
+	add_edge(graph,1,4,2);
+	add_edge(graph,2,4,4);
+	add_edge(graph,2,5,5);
+	add_edge(graph,3,4,4);
+	add_edge(graph,4,5,7);
+	print_edges(graph);
+	auto edges = get_edges_sorted(graph);
+	for(Edge& e:edges) {
+		cout<<e.from<<","<<e.to<<","<<e.weight<<endl;
+	}
+	auto span_tree = min_spanning_tree_kruskal(graph);
+	print_edges(span_tree);
+	auto span_tree2 = min_spanning_tree_djp(graph);
+	print_edges(span_tree2);
+
+}
